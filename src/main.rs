1c16d24afcdcba932de65eb40a6ac99df6a2eeaf
Corenthin <firelol2003@gmail.com>
2025-09-22 16:07:46 +0200

    Modify Hello

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use colored::*;
 
 fn main() {
-    println!("{}", "Hello, world!".green().bold());
+    println!("{}", "Hello, Corenthin".green().bold());
     println!("{}", "=== Footer ===".red());
 }
