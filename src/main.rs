3c41070d67590d3b4926ec3e50f9ab7ea96be7f7
Corenthin <firelol2003@gmail.com>
2025-09-22 16:13:45 +0200

    make conflict

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use colored::*;
 
 fn main() {
-    println!("{}", "Hello, Corenthin".green().bold());
+    println!("{}", "Hello, Corenthin L".green().bold());
     println!("{}", "Contact: contact@todo-app.dev".blue());
 }
