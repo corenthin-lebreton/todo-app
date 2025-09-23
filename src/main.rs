b20fb99d1f28a706168596e0d7a4ee12b99ca9f4
Corenthin <firelol2003@gmail.com>
2025-09-23 11:12:03 +0200

    modif

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,6 @@ fn main() {
     println!("{}", "Hello, Corenthin L".green().bold());
     println!("{}", "=== Footer ===".red());
     println!("{}", "Contact: contact@todo-app.dev".blue());
+
+    println!("{}", "Visit: https://todo-app.dev".yellow());
 }
